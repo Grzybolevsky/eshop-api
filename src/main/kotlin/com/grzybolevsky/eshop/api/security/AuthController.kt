3d46a64076f0cbcf5d8fe670c19fcc41a3acc20e
Szymon Żak <szymon436@gmail.com>
2022-09-25 01:27:46 +0200

    Forward session to client.

--- src/main/kotlin/com/grzybolevsky/eshop/api/security/AuthController.kt
+++ src/main/kotlin/com/grzybolevsky/eshop/api/security/AuthController.kt
@@ -1,6 +1,5 @@
 package com.grzybolevsky.eshop.api.security
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -8,13 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Profile("dev")
 @RequestMapping("/auth")
 @RestController
 class AuthController {
-    @Value("\${app.clientUrl:}")
-    private val clientUrl: String = ""
-
-    @Profile("dev")
     @GetMapping("/info")
     fun authInfo(@AuthenticationPrincipal principal: OAuth2User) = principal
 }
